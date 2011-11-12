35debf5eef648c0d55bf9ca1bfdefb5d3d253b72
Mikael Hultgren <blomma@gmail.com>
2011-11-13 00:40:44 +0100

    Markdown

--- init.el
+++ init.el
@@ -157,6 +157,14 @@
 (column-number-mode t)
 
 ;;;-----------------------------------------------------------------------------
+;;; Markdown mode
+;;;
+(autoload 'markdown-mode "markdown-mode"
+  "Major mode for editing Markdown files" t)
+(setq auto-mode-alist
+	  (cons '("\\.md" . markdown-mode) auto-mode-alist))
+
+;;;-----------------------------------------------------------------------------
 ;;; Whitespace mode
 ;;;
 
