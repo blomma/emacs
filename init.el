a8840e3fab32432be673155d40182ac61df1c723
Mikael Hultgren <blomma@gmail.com>
2012-09-03 21:03:29 +0200

    no message

--- init.el
+++ init.el
@@ -11,20 +11,20 @@
                  (not (equal f ".")))
         (add-to-list 'load-path name)))))
 
-        (add-subfolders-to-load-path "~/.emacs.d/vendor")
+(add-subfolders-to-load-path "~/.emacs.d/vendor")
+(add-to-list 'load-path "~/.emacs.d/lisp")
 
-        (defun add-subfolders-to-theme-load-path (parent-dir)
-          "Adds all first level `parent-dir' subdirs to the Emacs theme load path."
-          (dolist (f (directory-files parent-dir))
-            (let ((name (concat parent-dir "/" f)))
-              (when (and (file-directory-p name)
-                         (not (equal f ".."))
-                         (not (equal f ".")))
-                (add-to-list 'custom-theme-load-path name)))))
+(defun add-subfolders-to-theme-load-path (parent-dir)
+  "Adds all first level `parent-dir' subdirs to the Emacs theme load path."
+  (dolist (f (directory-files parent-dir))
+    (let ((name (concat parent-dir "/" f)))
+      (when (and (file-directory-p name)
+                 (not (equal f ".."))
+                 (not (equal f ".")))
+        (add-to-list 'custom-theme-load-path name)))))
 
 (add-subfolders-to-theme-load-path "~/.emacs.d/theme")
-
-(add-to-list 'load-path "~/.emacs.d/lisp")
+(add-to-list 'custom-theme-load-path "~/.emacs.d/theme")
 
 ;; Executables might be somewhere else
 (add-to-list 'exec-path "~/Opt/homebrew/bin")
@@ -41,15 +41,29 @@
 (if (not windows)
     (server-start))
 
-;; key board / input method settings
-(setq locale-coding-system 'utf-8)
+;; UTF-8 as default
+(prefer-coding-system 'utf-8)
+(set-default-coding-systems 'utf-8)
 (set-terminal-coding-system 'utf-8)
 (set-keyboard-coding-system 'utf-8)
-(set-selection-coding-system 'utf-8)
-(prefer-coding-system 'utf-8)
-(set-language-environment "UTF-8")       ; prefer utf-8 for language settings
-(set-input-method nil)                   ; no funky input for normal editing;
+;; backwards compatibility as default-buffer-file-coding-system
+;; is deprecated in 23.2.
+(if (boundp buffer-file-coding-system)
+    (setq buffer-file-coding-system 'utf-8)
+  (setq default-buffer-file-coding-system 'utf-8))
+
+;; Treat clipboard input as UTF-8 string first; compound text next, etc.
+(setq x-select-request-type '(UTF8_STRING COMPOUND_TEXT TEXT STRING))
+
+;; key board / input method settings
+;; (setq locale-coding-system 'utf-8)
+;; (set-selection-coding-system 'utf-8)
+;; (set-language-environment "UTF-8")       ; prefer utf-8 for language settings
+;; (set-input-method nil)                   ; no funky input for normal editing;
 (setq read-quoted-char-radix 10)         ; use decimal, not octal
+
+;; Dont pause display for input
+(setq redisplay-dont-pause t)
 
 ;; Mac specifik confs
 (when darwin
@@ -174,8 +188,8 @@
 
 (require 'deft)
 
-(setq deft-extension "txt")
-(setq deft-directory "~/Dropbox/Bucket/Notes")
+(setq deft-extension "md")
+(setq deft-directory "~/Dropbox/Bucket")
 (setq deft-text-mode 'markdown-mode)
 (setq deft-use-filename-as-title t)
 
@@ -186,6 +200,8 @@
 (require 'autopair)
 
 (autopair-global-mode)
+(setq autopair-autowrap t)
+(setq autopair-blink t)
 
 ;;;---------------------------------------------------------------------
 ;;; Save place
@@ -656,11 +672,14 @@ print a message in the minibuffer with the result."
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector [default bold shadow italic underline bold bold-italic bold])
  '(ansi-color-names-vector ["#3f3f3f" "#cc9393" "#7f9f7f" "#f0dfaf" "#8cd0d3" "#dc8cc3" "#93e0e3" "#dcdccc"])
+ '(ansi-term-color-vector [unspecified "#FFFFFF" "#d15120" "#5f9411" "#d2ad00" "#6b82a7" "#a66bab" "#6b82a7" "#505050"])
  '(background-color "#042028")
  '(background-mode dark)
  '(cursor-color "#708183")
- '(custom-enabled-themes (quote (twilight)))
- '(custom-safe-themes (quote ("f2de89c68dd88605a1a22467c77375b4f3b9530d6d866b1b2f1b05378a20242c" "c2e946f6382ad9476ed8f45a2971f46d1f291e10747090ee8cbe6537e0a34347" "da93340745b198a67a0daa41d72ec713c893e7cef9b8fe2d00f346d5755d3f37" "ecb58d3d27238e077bc48e013c9f5f09e2cab5442863339eb0c6ada97767bab7" "69a85c90b5ed858ccbb4426e836b522fc6c9ec99415a41e402fa059e5ddb2efd" "3602d01868d70a6ee600bb7d2cde42f9dba207d2ea77ed48409cc1bf27f3b860" "1e7e097ec8cb1f8c3a912d7e1e0331caeed49fef6cff220be63bd2a6ba4cc365" "fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" "870bd363bb2770316775ffa6e5938d73bee3adaba1f4d5b7b129533b3e0fed41" "0c8ffb272e65e2ffc49b0d5755d7db137931c5e3ed47890d7a6de07717eaa2e8" "7511ae742ae5e87bc096db346ab4694c1042a4a6035d7d15f4b86b4f2213c8d8" "b70e5b325e9c1b5672675343b35407a3d64b055b6fca5846f55232127693cb2c" "3f43263bd8540fcba50c16b0e21f788a4a73fb06cbfc8fb61afeab625ff61ec2" "edb0e9dce76acf08243762d30683293812c838773f0e9f41b7e6baf904776d6c" default)))
+ '(custom-enabled-themes (quote (twilight-bright)))
+ '(custom-safe-themes (quote ("06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" "5e1d1564b6a2435a2054aa345e81c89539a72c4cad8536cfe02583e0b7d5e2fa" "fca8ce385e5424064320d2790297f735ecfde494674193b061b9ac371526d059" "159bb8f86836ea30261ece64ac695dc490e871d57107016c09f286146f0dae64" "e9a1226ffed627ec58294d77c62aa9561ec5f42309a1f7a2423c6227e34e3581" "211bb9b24001d066a646809727efb9c9a2665c270c753aa125bace5e899cb523" "6615e5aefae7d222a0c252c81aac52c4efb2218d35dfbb93c023c4b94d3fa0db" "27b53b2085c977a8919f25a3a76e013ef443362d887d52eaa7121e6f92434972" "967c58175840fcea30b56f2a5a326b232d4939393bed59339d21e46cf4798ecf" "6938c51c0a89f078c61b979af23ae4c32204458f16a6a08c1a683ab478a7bc6b" "f2de89c68dd88605a1a22467c77375b4f3b9530d6d866b1b2f1b05378a20242c" "c2e946f6382ad9476ed8f45a2971f46d1f291e10747090ee8cbe6537e0a34347" "da93340745b198a67a0daa41d72ec713c893e7cef9b8fe2d00f346d5755d3f37" "ecb58d3d27238e077bc48e013c9f5f09e2cab5442863339eb0c6ada97767bab7" "69a85c90b5ed858ccbb4426e836b522fc6c9ec99415a41e402fa059e5ddb2efd" "3602d01868d70a6ee600bb7d2cde42f9dba207d2ea77ed48409cc1bf27f3b860" "1e7e097ec8cb1f8c3a912d7e1e0331caeed49fef6cff220be63bd2a6ba4cc365" "fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" "870bd363bb2770316775ffa6e5938d73bee3adaba1f4d5b7b129533b3e0fed41" "0c8ffb272e65e2ffc49b0d5755d7db137931c5e3ed47890d7a6de07717eaa2e8" "7511ae742ae5e87bc096db346ab4694c1042a4a6035d7d15f4b86b4f2213c8d8" "b70e5b325e9c1b5672675343b35407a3d64b055b6fca5846f55232127693cb2c" "3f43263bd8540fcba50c16b0e21f788a4a73fb06cbfc8fb61afeab625ff61ec2" "edb0e9dce76acf08243762d30683293812c838773f0e9f41b7e6baf904776d6c" default)))
+ '(fci-rule-character-color "#d9d9d9")
  '(fci-rule-color "#383838")
  '(foreground-color "#708183"))
